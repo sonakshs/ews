59f94401042dc8969e489cf9ff31a856b8fc989c
pulkit <kpulkit291@gmail.com>
2018-08-12 00:29:30 +0530

    added files

--- src/Components/ViewTransactions/index.js
+++ src/Components/ViewTransactions/index.js
@@ -2,57 +2,61 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import storeHash from "../storeHash.js";
 class ViewTransactions extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            arr:[]
-         }
-         this.renderState=this.renderState.bind(this);  
-    }
-    renderState(){
-        var that = this;
-        for(var i=0;i<3;i++){
-            storeHash.methods.getHash(this.props.account,i).call().then(function(res){
-               that.setState({
-                arr: [...that.state.arr,res]
-              }) 
-            })
+   constructor(props) {
+       super(props);
+       this.state = {
+           arr:[]
         }
-    }
-    componentDidMount(){
-        this.renderState();
-    }
-    render() { 
-        const rows= this.state.arr.map((transactionObject,index) => {
-        return (
-            <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{transactionObject}</td>
-                <td>{transactionObject.size}</td>
-            </tr>
-        )
-    })
-        return ( 
-            <div className="container">
-                <table className="highlight">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Item Name</th>
-                        <th>Item Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+        this.renderState=this.renderState.bind(this);
+   }
+   renderState(){
+       var that = this;
+       for(var i=0;i<3;i++){
+           storeHash.methods.getHash(this.props.account,i).call().then(function(res){
+              console.log(res);
+              if(res["0"]){
+                that.setState({
+                    arr: [...that.state.arr,res]
+                  })
+              }
+           
+           })
+       }
+   }
+   componentDidMount(){
+       this.renderState();
+   }
+   render() {
+       const rows= this.state.arr.map((transactionObject,index) => {
+       return (
+           <tr key={index}>
+               <td>{index + 1}</td>
+               <td>{transactionObject["0"]}</td>
+               <td>{transactionObject["1"]}</td>
+           </tr>
+       )
+   })
+       return (
+           <div className="container">
+               <table className="highlight">
+                   <thead>
+                   <tr>
+                       <th>Name</th>
+                       <th>Item Name</th>
+                       <th>Item Price</th>
+                   </tr>
+                   </thead>
+                   <tbody>
+                       {rows}
+                   </tbody>
+               </table>
+           </div>
+       );
+   }
 }
 
 ViewTransactions.propTypes = {
-    account: PropTypes.string.isRequired
+   account: PropTypes.string.isRequired
 }
- 
+
 export default ViewTransactions;
