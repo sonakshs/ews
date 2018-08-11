873c92d758894d9571724c995ef5d1135970e12a
Sonaksh <sonaksh.saraswat@tryscribe.com>
2018-08-11 17:56:00 +0530

    ui changes + added jquery

---
+++ src/Components/Dashboard/UploadSection/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class UploadSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Upload Files Here.
+            </div>
+        );
+    }
+}
+ 
+export default UploadSection;
--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ViewSection from './ViewSection';
+import UploadSection from './UploadSection';
 
 class Dashboard extends Component {
     constructor(props) {
@@ -8,8 +10,20 @@ class Dashboard extends Component {
     }
     render() { 
         return ( 
-            <div>
+            <div style={{backgroundColor: '#707ce6'}}>
                 Dashboard account id = {this.props.account}
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12">
+                            <UploadSection/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <ViewSection/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
