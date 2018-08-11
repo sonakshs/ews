217af56e6c9ecda1d4ebc9d3e3b79625a5b82b0d
Sonaksh <sonaksh.saraswat@tryscribe.com>
2018-08-11 20:57:30 +0530

    directory structure modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,89 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import web3 from "./Components/web3.js";
 
 import './App.css';
-import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
-import LogoutModal from './Components/Dashboard/logoutModal';
+import ViewTransactions from './Components/ViewTransactions';
 
 class App extends Component {
-  render() {
-    const Homepage = () => (
-      <Router>
-        <div>
-          <nav>
-            <div className="nav-wrapper" style={{backgroundColor: '#062d4c'}}>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/dashboard">Dashboard</Link>
-                </li>
-                <li>
-                  <a className="modal-trigger" href="#logoutModal">Logout</a>
-                  <LogoutModal/>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <Route exact path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-        </div>
-      </Router>
-    );
-    
-    return (
-      <div className="App">
-        <Homepage/>
-      </div>
-    );
+    constructor(props) {
+      super(props);
+      this.state = { 
+          account: "",
+          loggedIn: false
+      }
+      this.changeAccount = this.changeAccount.bind(this);
+      this.changeStatus = this.changeStatus.bind(this);
   }
+    changeAccount(value){
+      this.setState({account:value})
+    }
+    changeStatus(value){
+        var newValue = value
+        this.setState({loggedIn:newValue})
+    }
+  
+    componentDidMount(){
+      var that = this
+      web3.eth.getAccounts(function(error,result) {
+          if(!result || result.lenght <= 0){
+              that.changeStatus(false);
+          }
+          else if(result.length>0)
+          {   
+              that.changeStatus(true);
+              that.changeAccount(result[0]);
+          }
+      });        
+    }
+    render() {
+      const navLoggedIn = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>      
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/viewTransactions">View Transactions</Link>
+          </li>
+          <li>
+            <Link to="#">Logout</Link>
+          </li>
+        </ul>
+      )
+      const navLoggedOut = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>      
+            <Link to="/#">Login</Link>
+          </li>
+        </ul>
+      )
+      const nav = (
+        this.state.loggedIn ? navLoggedIn : navLoggedOut
+      )
+      const Homepage = () => (
+        <div>
+          <h2>Intro Here.</h2>
+        </div>
+      );
+      return (
+        <div className="App">
+          <Router>
+          <div>
+            <nav>
+              <div className="nav-wrapper" style={{backgroundColor: '#062d4c'}}>
+                <a href="#" style={{float: 'left', fontSize: '4em', marginLeft: '0.5em', marginTop: '0.052em'}}>ews</a>
+                {nav}
+              </div>
+            </nav>
+            <Route exact path="/" component={Homepage} />  
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/viewTransactions" component={ViewTransactions} />
+          </div>
+          </Router>
+        </div>
+      );
+    }
 }
 
 export default App;
---
+++ src/Components/ViewTransactions/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ViewTransactions extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                View Transactions Here.
+            </div>
+        );
+    }
+}
+ 
+export default ViewTransactions;
