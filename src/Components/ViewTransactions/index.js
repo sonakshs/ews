15854dea2d7101e6eb0c510e6710b5554444c184
pulkit <kpulkit291@gmail.com>
2018-08-11 23:32:28 +0530

    Added files

--- src/Components/ViewTransactions/index.js
+++ src/Components/ViewTransactions/index.js
@@ -4,11 +4,26 @@ import storeHash from "../storeHash.js";
 class ViewTransactions extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
-        storeHash.methods.getHash("0xe53ada4fe62c8972890d75cbc4c4d2fe6317e84d",2).call().then(function(res){
-            console.log(res);
-        })
-        console.log(props);
+        this.state = { 
+            arr:[]
+         }
+         this.renderState=this.renderState.bind(this);
+        //implement a loop here to get the list of documents  
+    }
+    renderState(){
+        var that = this;
+        var temp=[];
+        for(var i=0;i<3;i++){
+            storeHash.methods.getHash("0xe53ada4fe62c8972890d75cbc4c4d2fe6317e84d",i).call().then(function(res){
+               console.log(res);
+               that.setState({
+                arr: [...that.state.arr,res]
+              }) 
+            })
+        }
+    }
+    componentDidMount(){
+        this.renderState();
     }
     getTransactionCount() {
        storeHash.methods.getHash().call().then(function(res){
@@ -19,6 +34,9 @@ class ViewTransactions extends Component {
         return ( 
             <div>
                 View Transactions Here.
+                <button>{this.state.arr[0]}</button>
+                <button>{this.state.arr[1]}</button>
+                <button>{this.state.arr[2]}</button>
             </div>
         );
     }
